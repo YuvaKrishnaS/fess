d4c98dc1bbf2210d31d38457e39a180a3c1a39e9
Krishna <krishnathecodernaveen@gmail.com>
2026-02-11 20:01:30 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/confes/fessv2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.confes.fessv2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  //Backgrounds
+  static const Color backgroundMain = Color(0xFF000000);
+  static const Color elevated = Color(0xFF303030);
+  
+  // Text
+  static const Color textPrimary = Color(0xFFE7E9EA);
+  static const Color textSecondary = Color(0xFF9E9E9E);
+  static const Color hintText = Color(0xFF616161);
+
+  // Accents
+  static const Color accentPrimary = Color(0xFF1DE9B6);
+  static const Color accentSecondary = Color(0xFF7C4DFF);
+  static const Color accentGlow = Color(0xFFC8BEE4);
+
+  // Errors
+  static const Color errorLight = Color(0xFFFF5F57);
+  static const Color errorDark = Color(0xFFFF9C9E);
+}
