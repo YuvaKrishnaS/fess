4e2e4f8b006b4e0797ab8d74d065bba66c289ed5
Krishna <krishnathecodernaveen@gmail.com>
2026-06-15 23:05:41 +0530

    update(typography, colors): added DMSerif-italic font to pubspec.yaml and app typography, added some new colors to app_colrs - surfaceSubtle, borderSubtle, accentDim

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -7,17 +7,22 @@ class AppColors {
   // Backgrounds
   static const Color backgroundMain = Color(0xFF000000);
   static const Color elevated = Color(0xFF303030);
+  static const Color surfaceSubtle = Color(0xFF111118);
 
   // Text
   static const Color textPrimary = Color(0xFFE7E9EA);
   static const Color textSecondary = Color(0xFF9E9E9E);
   static const Color hintText = Color(0xFF616161);
 
+  // Borders
   static const Color border = Color(0xFF2A2A3E); // subtle dark border
+  static const Color borderSubtle = Color(0xFF1A1A28);
+
   // Accents
   static const Color accentPrimary = Color(0xFF1DE9B6);
   static const Color accentSecondary = Color(0xFF7C4DFF);
   static const Color accentGlow = Color(0xFFC8BEE4);
+  static const Color accentDim = Color(0xFF0D3D30);
 
   // Errors
   static const Color errorLight = Color(0xFFFF5F57);
